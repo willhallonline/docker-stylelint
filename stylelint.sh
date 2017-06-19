7bf0610e370dd519a498b7a87bff6721ad1dc079
Will Hall <will@willhallonline.co.uk>
2017-06-19 10:25:14 +0100

    Create stylelint.sh
    Stylelint.sh as an entrypoint for the container, checking the existence of .stylelintrc

---
+++ stylelint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Run stylelint, copying in specific styleint rules
+
+# Test if already has a .stylelintrc file. If not, add a temporary one.
+if [ -f "/app/.stylelintrc" ]
+then
+  stylelint_rc=true
+else
+  echo '{"extends": "/usr/local/lib/node_modules/stylelint-config-standard"}' > /app/.stylelintrc
+fi
+
+stylelint "$@"
+
+# Remove temporary .stylelintrc file if created in this process 
+if [ "$stylelint_rc" = true ]
+then
+  echo "--- Stylelint Complete ---"
+else
+  rm /app/.stylelintrc
+  echo "--- Stylelint Complete ---"
+fi
